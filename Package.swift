4ceb46421090702c8d28f0824fbeb9b6fa5c2fe6
runner <runner@Mac-1664997164303.local>
2022-10-05 19:20:29 +0000

    KMM SPM package file revert

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "https://api.github.com/repos/touchlab/KMMBridgeSampleKotlin/releases/assets/80072150.zip"
-let remoteKotlinChecksum = "6d0bf2841e9a44e3eba29779c1c34a62959e175f7a44c2a392dcd09082e85052"
+let remoteKotlinUrl = "https://api.github.com/repos/touchlab/KMMBridgeSampleKotlin/releases/assets/80062729.zip"
+let remoteKotlinChecksum = "86225dfc2afbe451be69a081bf0e3895f7ee491c699e98f2af3017f21793bbf4"
 let packageName = "shared"
 
 let package = Package(
