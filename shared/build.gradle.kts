396ff19771d8bf7ad607495e3188965d56212a17
Michael Friend <michaelf7777@gmail.com>
2022-10-05 13:48:53 -0400

    KMM SPM package release for 0.1.6

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
-    id("co.touchlab.faktory.kmmbridge") version "0.1.14-SNAPSHOT"
+    id("co.touchlab.faktory.kmmbridge") version "0.1.15-SNAPSHOT"
     kotlin("native.cocoapods")
 }
 
