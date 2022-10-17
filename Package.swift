2c9502b07ed3773128d0c5c62f16bfa61e22164e
Michael Friend <michaelf7777@gmail.com>
2022-10-17 11:32:57 -0400

    KMM SPM package release for 0.6.1666020762531

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "https://touchlabtest.jfrog.io/artifactory/faktorygradle/KMMBridgeSampleKotlin/shared-kmmbridge/0.6.1666020641796/shared-kmmbridge-0.6.1666020641796.zip"
+let remoteKotlinUrl = "https://touchlabtest.jfrog.io/artifactory/faktorygradle/KMMBridgeSampleKotlin/shared-kmmbridge/0.6.1666020762531/shared-kmmbridge-0.6.1666020762531.zip"
 let remoteKotlinChecksum = "5a9231f0a02283aee7722711fd79f71bed8c3e73ee07abe8c9f944e3bf8705c2"
 let packageName = "shared"
 
