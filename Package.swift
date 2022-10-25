4d4bcccbd5d7e20f357e796fb94a1cb8673888aa
Paul Hawke <paul.hawke@gmail.com>
2022-10-25 08:19:26 -0500

    KMM SPM package release for 0.6.1666703897449

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "https://gitlab.com/api/v4/projects/40398438/packages/maven/KMMBridgeSampleKotlin/shared-kmmbridge/0.6.1666294644675/shared-kmmbridge-0.6.1666294644675.zip"
-let remoteKotlinChecksum = "130cc2e849dcae99658b773cfe58e6f7b0cfed3825af9b70c8fa322f5cc9191f"
+let remoteKotlinUrl = "https://gitlab.com/api/v4/projects/40400272/packages/maven/KMMBridgeSampleKotlin/shared-kmmbridge/0.6.1666703897449/shared-kmmbridge-0.6.1666703897449.zip"
+let remoteKotlinChecksum = "bd8f21b6b8a71f73ffbb41d0af2906c570f4796b081266848a012611ca1703be"
 let packageName = "shared"
 
 let package = Package(
